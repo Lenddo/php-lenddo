bbc27b6b028a1d7449403a8b4636fc3f0155b42c
Howard Lince III <howard@lenddo.com>
2016-03-18 11:01:51 +0800

    Fixing array dereferencing incompatibility for 5.3

--- tests/cases/WhiteLabelClientTest.php
+++ tests/cases/WhiteLabelClientTest.php
@@ -38,9 +38,10 @@ class WhiteLabelClientTest extends \Lenddo\tests\cases\BaseClientTest
 	public function testClientInstantiation()
 	{
 		$client = $this->_buildServiceClient();
+		$hosts = $client->getHosts();
 
 		// Ensure the proper "default" host is being defined here.
-		$this->assertEquals($this->_getExpectedBaseUri(), $client->getHosts()['network_service']);
+		$this->assertEquals($this->_getExpectedBaseUri(), $hosts['network_service']);
 	}
 
 	public function testPartnerToken()
