a464971bdba909ca172376e8e1237bf0f2217e18
Howard Lince III <howard@lenddo.com>
2016-04-18 17:14:35 +0800

    adding unit tests for the Verificaiton class.

---
+++ tests/cases/VerificationTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Lenddo\tests\cases;
+
+use Lenddo\Verification;
+
+class VerificationTest extends \PHPUnit_Framework_TestCase {
+	public function testVerificationInstantiation() {
+		$this->assertInstanceOf('Lenddo\Verification', new Verification());
+	}
+
+	public function testSetMethods() {
+		$expectedSetMethods = array(
+			'FirstName' => 'fn',
+			'MiddleName' => 'mn',
+			'LastName' => 'ln',
+			'DateOfBirth' => '1988-05-04',
+			'Employer' => 'Lenddo',
+			'MobilePhone' => 'mp',
+			'University' => 'u'
+		);
+
+		$verification = new Verification();
+
+		foreach($expectedSetMethods as $verificationKey => $value) {
+			$verification->{'set' . $verificationKey}($value);
+		}
+
+		$this->assertEquals($verification->export(), array(
+			'name' => array(
+				'first' => 'fn',
+				'middle' => 'mn',
+				'last' => 'ln'
+			),
+			'date_of_birth' => '1988-05-04',
+			'employer' => 'Lenddo',
+			'phone' => array(
+				'mobile' => 'mp'
+			),
+			'university' => 'u'
+		));
+	}
+
+	public function testNameOnly() {
+		$verification = new Verification();
+		$verification->setFirstName('Howard')
+			->setLastName('Lince III');
+
+		$this->assertEquals($verification->export(), array(
+			'name' => array(
+				'first' => 'Howard',
+				'last' => 'Lince III'
+			)
+		));
+	}
+}
